020b5e030b7f3a764136247c61db80fb7a3c9207
bangulsangyee <underfellunder@gmail.com>
2023-10-25 16:52:22 +0900

    Resolution selection
    해상도 선택 기능 업데이트

---
+++ Assets/C#/UI/SettingUI.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SettingUI : MonoBehaviour
+{
+    [SerializeField] private GameObject SettingPanel;
+    private FullScreenMode screenMode;
+    public Toggle fullscreenBtn;
+    public Dropdown resolutionDropdown;
+    private List<Resolution> resolutions = new List<Resolution>();
+    public int resolutionNum;
+    
+
+    void Start()
+    {
+        InitUI();
+    }
+
+    private void Update()
+    {
+        // Cursor.lockState = CursorLockMode.None;
+    }
+
+    void InitUI()
+    {
+        for (int i = 0; i < Screen.resolutions.Length; i++)
+        {
+            if (Screen.resolutions[i].refreshRate >= 60)
+            {
+                resolutions.Add(Screen.resolutions[i]);
+            }
+        }
+        
+        int optionNum = 0;
+        // resolutions.AddRange(Screen.resolutions);
+        resolutionDropdown.options.Clear();
+
+        foreach (Resolution item in resolutions)
+        {
+            Dropdown.OptionData option = new Dropdown.OptionData();
+            option.text = item.width + " x " + item.height + " " + item.refreshRate + "hz";
+            resolutionDropdown.options.Add(option);
+
+            if (item.width == Screen.width && item.height == Screen.height)
+            {
+                resolutionDropdown.value = optionNum;
+            }
+
+            optionNum++;
+
+        }
+        resolutionDropdown.RefreshShownValue();
+
+        fullscreenBtn.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow) ? true : false;
+    }
+
+    public void DropboxOptionChange(int x)
+    {
+        resolutionNum = x;
+    }
+
+    public void FullScreanBtn(bool isFull)
+    {
+        screenMode = isFull ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
+    }
+    
+    public void OkBtnClick()
+    {
+        Screen.SetResolution(resolutions[resolutionNum].width
+            , resolutions[resolutionNum].height,
+            screenMode);
+    }
+
+    public void OnExitBtnClick()
+    {
+        SettingPanel.SetActive(false);
+        Time.timeScale = 1.0f;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
