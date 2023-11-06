d5527af45c58ae9d6b2139c9fc7789fdb75cb344
bangulsangyee <underfellunder@gmail.com>
2023-11-06 20:22:25 +0900

    SettingResolution Update
    if settingResolution panel is On
    if you press "O"(Alphabet) once again the settingResolution panel turn off

--- Assets/C#/UI/OnSettingResolution.cs
+++ Assets/C#/UI/OnSettingResolution.cs
@@ -6,14 +6,30 @@ using UnityEngine;
 public class OnSettingResolution : MonoBehaviour
 {
     [SerializeField] private GameObject panel;
-    
+    [SerializeField] private bool isOn;
+
+    private void Start()
+    {
+        isOn = false;
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.O))
+        if (Input.GetKeyDown(KeyCode.O) && isOn == false)
         {
+            isOn = true;
             panel.SetActive(true);
             Time.timeScale = 0f;
             Cursor.lockState = CursorLockMode.None;
         }
+        
+        else if (Input.GetKeyDown(KeyCode.O) && isOn == true)
+        {
+            isOn = false;
+            panel.SetActive(false);
+            Time.timeScale = 1.0f;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        
     }
 }
