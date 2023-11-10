f381d5d25dbe0a48a549749dd2d245fe2dafa720
kwonso8520 <103422079+kwonso8520@users.noreply.github.com>
2023-11-10 10:52:19 +0900

    .

--- Assets/C#/Rock.cs
+++ Assets/C#/Rock.cs
@@ -17,7 +17,7 @@ public class Rock : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && other.gameObject.tag == "Bunker")
         {
             playerController.Hit(enemyData.Damage);
             Destroy(gameObject);
